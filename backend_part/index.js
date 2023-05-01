ef54ef4302b39c13fd56ba4df07ef396f61fdee8
priyankar-laptop <priyankarkoley@gmail.com>
2023-05-01 12:33:15 +0530

    Express server basic setup

--- backend_part/index.js
+++ backend_part/index.js
@@ -1,14 +1,14 @@
 const connectToMongo = require("./db");
-const express = require("express");
+const express = require('express')
 
 connectToMongo();
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("hey");
-});
+// // AVAILABLE ROUTES
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log("Server Started");
-});
+  console.log(`Example app listening on port ${port}`)
+})
