c89b63399342e118d2938acddb97dd453da8dd58
priyankar-laptop <priyankarkoley@gmail.com>
2023-05-04 08:56:53 +0530

    Add Notes endpoint added

--- backend_part/routes/notes.js
+++ backend_part/routes/notes.js
@@ -11,6 +11,25 @@ router.get('/get', fetchUser, async(req, res) => {
     res.json(notes);
 })
 
-
+//---------------------------------------------------------------------------
+//Endpoint to ADD notes, using POST | LOGIN REQUIRED
+router.post('/add',[
+    body("title", "Title must have atleast 2 characters").isLength({min: 2,}),
+    body("description", "Description must have atleast 5 characters").isLength({min: 5,}),
+  ], fetchUser, async(req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        return res.status(400).json({errors: err.array()});
+    }
+    try {
+        const {title, description, tag} = req.body
+        let note = new Note({title, description, tag, usertoken : req.user.id});
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+})
 
 module.exports = router;
